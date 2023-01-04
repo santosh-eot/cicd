72513be6485f86fe5f8cea2389cab0bdc5380227
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-04 20:53:04 +0530

    22 commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 1212;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
