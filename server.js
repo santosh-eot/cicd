2aaaf111b5cf1a38eff5ab5ebb5e9c11d88d51ff
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-04 20:51:07 +0530

    21 commit

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 1212;
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
 app.get('/', (req, res)=>{
-    res.send(`hello from ${PORT}, welcome `)
+    res.send(`hello from ${PORT}, welcome to cicd`)
 })
