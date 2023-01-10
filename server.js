d27975d2c19be7cb10236bb9f4d7fc46e71240f5
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-10 19:05:39 +0530

    server updated with params

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
 app.get('/', (req, res)=>{
-    res.send(`update on host2`)
+    res.send(`update on host1 with params`)
 })
