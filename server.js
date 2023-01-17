b2d3a9562fcca2ce94dec4cd51ef79cad569b312
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-17 20:16:13 +0530

    removed build

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
 app.get('/', (req, res)=>{
-    res.send(`update on host1 with params, env1: ${process.env.TEST1} en2: ${process.env.TEST2}`)
+    res.send(`update on host1 with params, dynamic env1: ${process.env.TEST1} en2: ${process.env.TEST2}`)
 })
 
