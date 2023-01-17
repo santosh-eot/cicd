5467c79b9079cc8175ac9ec54119b710ee12d7e9
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-17 19:59:45 +0530

    dynamic env file

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
 app.get('/', (req, res)=>{
-    res.send(`update on host1 with params, Hello from ${process.env.TEST}`)
+    res.send(`update on host1 with params, env1: ${process.env.TEST1} en2: ${process.env.TEST2}`)
 })
 
