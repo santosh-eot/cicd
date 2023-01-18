5b4855c950e311237ce5424e0b9646071e0a31c1
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-18 20:12:49 +0530

    removed actions

--- server.js
+++ server.js
@@ -18,23 +18,28 @@ app.get('/', (req, res)=>{
 app.post('/create-yml', async(req, res)=>{
     try {
         let {server, host, key, env} = req.body
+        let str = ''
+        for(let i in env){
+            str += `${i}=${env[i]}\\n`
+        }
         let d
         if(req.query.type == 'initial'){
             let {clone} = req.body
             let folder = clone.split('/')[1]
             folder = folder.split('.')[0]
             var data = fs.readFileSync('content/initial.txt', 'utf8')
-            d = data.split(' ')
             let serverIndex = d.indexOf('serverValue')
             let hostIndex = d.indexOf('hostValue')
             let keyIndex = d.indexOf('keyValue')
             let cloneIndex = d.indexOf('cloneUrl')
             let folderIndex = d.indexOf('~/folder')
+            let envIndex = d.indexOf('\"envValue\"')
             d[serverIndex] = `${server} `
             d[hostIndex] = "${{ secrets."+host+"}} "
             d[keyIndex] = "${{ secrets."+key+"}} "
             d[cloneIndex] = clone
             d[folderIndex] = `~/${folder}`
+            d[envIndex] = str
             writeYamlFile.sync(`.github/workflows/${server}-initial.yaml`, d.join(' '))
         }else{
             var data = fs.readFileSync('content/cicd.txt', 'utf8')
@@ -47,7 +52,7 @@ app.post('/create-yml', async(req, res)=>{
             d[keyIndex] = "${{ secrets."+key+"}} ";
             writeYamlFile.sync(`.github/workflows/${server}-cicd.yaml`, d.join(' '))
         }
-        res.send('done')
+        // res.send('done')
     } catch (error) {
         console.log(error)
     }
