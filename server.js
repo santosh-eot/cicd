f3e7ce173cc7b55e06a2f0c679b4d71ce0588332
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-01-24 21:23:59 +0530

    added test case

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
 app.get('/', (req, res)=>{
-    // res.send(`dynamic environment variable 1: ${process.env.TEST1} and environment variable 2: ${process.env.TEST2}`)
     res.send(`hello world`)
 })
 
