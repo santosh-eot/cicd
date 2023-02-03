8ccaa918d04b8018a2c603c2103e227d68cb508e
Santosh Kashyap <santosh.kashyap.extern@diconium.com>
2023-02-03 19:54:53 +0530

    added console

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
 
 app.get('/', (req, res)=>{
+    console.log(`hello world form port ${PORT}`)
     res.send(`hello world form port ${PORT}`)
 })
 
